9e15ae7cded2ac133aa7345a0142fe904f7619da
Gabriel Rosa <gabokuck1990@gmail.com>
2021-12-16 22:36:49 -0600

    se diseñaron los componentes

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Pokemon Picture</h1>
+
+  <div class="pokemon-container">
+    <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+    <!-- <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon"> -->
+  </div>
 </template>
 
 <script>
@@ -8,6 +12,26 @@ export default {
 }
 </script>
 
-<style>
+<style scope>
+
+  .pokemon-container {
+    height: 200px;
+  }
+  img {
+    height: 200px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+  }
+  .hidden-pokemon {
+      filter: brightness(0);
+  }
+
 
 </style>
